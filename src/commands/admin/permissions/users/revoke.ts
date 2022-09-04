3d43122b65dc274556a953bafd9d0b7682d1e043
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-04 16:03:06 +0200

    Created admin:permissions:users:revoke command

--- src/commands/admin/permissions/users/revoke.ts
+++ src/commands/admin/permissions/users/revoke.ts
@@ -3,11 +3,11 @@ import { StashConnector } from "stash-connector";
 import { BaseCommand, BuildFlags } from "../../../../libs/core/baseCommand";
 import { StashCLIResponse } from "../../../../libs/core/stashResponse";
 
-export default class Update extends BaseCommand {
+export default class Revoke extends BaseCommand {
     static description = 'Promote or demote the global permission level of a user.';
     static examples = [
-        `$ stash admin:permissions:users:update -a MyStashAlias --name Username --permission 'ADMIN' --json`,
-        `$ stash admin:permissions:users:update -a MyStashAlias --name OtherUser --permission PROJECT_CREATE`,
+        `$ stash admin:permissions:users:revoke -a MyStashAlias --name Username --json`,
+        `$ stash admin:permissions:users:revoke -a MyStashAlias --name OtherUser`,
     ];
     static flags = {
         ...BaseCommand.flags,
@@ -17,21 +17,14 @@ export default class Update extends BaseCommand {
             required: true,
             name: 'Name'
         }),
-        permission: Flags.string({
-            description: 'The permission to grant',
-            type: "option",
-            options: ['LICENSED_USER', 'PROJECT_CREATE', 'ADMIN', 'SYS_ADMIN'],
-            required: true,
-            name: 'Permission'
-        }),
     };
     async run(): Promise<StashCLIResponse<any>> {
         const response = new StashCLIResponse<any>();
         const connector = new StashConnector(this.localConfig.getConnectorOptions(this.flags.alias));
         try {
-            await connector.admin.permissions().users().update(this.flags.name, this.flags.permission);
+            await connector.admin.permissions().users().revoke(this.flags.name);
             response.status = 0;
-            response.message = this.getRecordUpdatedText('User Permissions');
+            response.message = 'User Permissions Revoked successfully';
             console.log(response.message);
         } catch (error) {
             this.processError(response, error);
