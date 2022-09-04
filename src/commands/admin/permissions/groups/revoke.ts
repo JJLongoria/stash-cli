f10f70270e67067fdec267acf3634552bc4b2f54
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-04 16:24:51 +0200

    Created admin:permissions:groups:revoke command

--- src/commands/admin/permissions/groups/revoke.ts
+++ src/commands/admin/permissions/groups/revoke.ts
@@ -3,11 +3,11 @@ import { StashConnector } from "stash-connector";
 import { BaseCommand, BuildFlags } from "../../../../libs/core/baseCommand";
 import { StashCLIResponse } from "../../../../libs/core/stashResponse";
 
-export default class Update extends BaseCommand {
-    static description = 'Promote or demote the global permission level of a group.';
+export default class Revoke extends BaseCommand {
+    static description = 'Revoke all global permissions for a group.';
     static examples = [
-        `$ stash admin:permissions:groups:update -a MyStashAlias --name groupName --permission 'ADMIN' --json`,
-        `$ stash admin:permissions:groups:update -a MyStashAlias --name otherGroup --permission PROJECT_CREATE`,
+        `$ stash admin:permissions:groups:revoke -a MyStashAlias --name groupName --json`,
+        `$ stash admin:permissions:groups:revoke -a MyStashAlias --name otherGroup`,
     ];
     static flags = {
         ...BaseCommand.flags,
@@ -17,21 +17,14 @@ export default class Update extends BaseCommand {
             required: true,
             name: 'Name'
         }),
-        permission: Flags.string({
-            description: 'The permission to grant',
-            type: "option",
-            options: ['LICENSED_USER', 'PROJECT_CREATE', 'ADMIN', 'SYS_ADMIN'],
-            required: true,
-            name: 'Permission'
-        }),
     };
     async run(): Promise<StashCLIResponse<any>> {
         const response = new StashCLIResponse<any>();
         const connector = new StashConnector(this.localConfig.getConnectorOptions(this.flags.alias));
         try {
-            await connector.admin.permissions().groups().update(this.flags.name, this.flags.permission);
+            await connector.admin.permissions().groups().revoke(this.flags.name);
             response.status = 0;
-            response.message = this.getRecordUpdatedText('Group Permissions');
+            response.message = 'Group Permissions Revoked successfully';
             console.log(response.message);
         } catch (error) {
             this.processError(response, error);
