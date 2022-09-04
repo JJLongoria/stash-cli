2be8b8e84af2141621023614a497fe326f190624
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-04 23:53:32 +0200

    Created projects:repos:get command

--- src/commands/projects/repos/get.ts
+++ src/commands/projects/repos/get.ts
@@ -5,12 +5,11 @@ import { StashCLIResponse } from "../../../libs/core/stashResponse";
 import { RepositoryColumns } from "../../../libs/core/tables";
 import { UX } from "../../../libs/core/ux";
 
-export default class Create extends BaseCommand {
-    static description = 'Create a new repository. Requires an existing project in which this repository will be created. ' + UX.processDocumentation('<doc:Repository>');
+export default class Get extends BaseCommand {
+    static description = 'Retrieve the repository matching the supplied projectKey and repositorySlug. ' + UX.processDocumentation('<doc:Repository>');
     static examples = [
-        `$ stash projects:repos:create -a MyStashAlias --project "ProjectKey" --name "MyRepo" --csv`,
-        `$ stash projects:repos:create -a MyStashAlias --project "ProjectKey" --name "MyRepo" --json`,
-        `$ stash projects:repos:create -a MyStashAlias --project "ProjectKey" --name "MyRepo" --scm git`,
+        `$ stash projects:repos:get -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --csv`,
+        `$ stash projects:repos:get -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --json`,
     ];
     static flags = {
         ...BaseCommand.flags,
@@ -18,33 +17,24 @@ export default class Create extends BaseCommand {
         extended: BuildFlags.extended,
         alias: BuildFlags.alias,
         project: Flags.string({
-            description: 'The Project key to create the repository',
+            description: 'The Project key to retrieve the repository',
             required: true,
             name: 'Project'
         }),
-        name: Flags.string({
-            description: 'The Repository name to create',
+        slug: Flags.string({
+            description: 'The Repository slug to retrieve',
             required: true,
-            name: 'Name'
-        }),
-        scm: Flags.string({
-            description: 'The Repository SCM Id.',
-            required: false,
-            name: 'ScmId',
-            default: 'git'
+            name: 'Slug'
         }),
     };
     async run(): Promise<StashCLIResponse<Repository>> {
         const response = new StashCLIResponse<Repository>();
         const connector = new StashConnector(this.localConfig.getConnectorOptions(this.flags.alias));
         try {
-            const result = await connector.projects.repos(this.flags.project).create({
-                name: this.flags.name,
-                scmId: this.flags.scmId,
-            })
+            const result = await connector.projects.repos(this.flags.project).get(this.flags.slug);
             response.result = result;
             response.status = 0;
-            response.message = this.getRecordCreatedText('Repository');
+            response.message = this.getRecordRetrievedText('Repository');
             console.log(response.message);
             this.ux.table<Repository>([result], RepositoryColumns, {
                 csv: this.flags.csv,
