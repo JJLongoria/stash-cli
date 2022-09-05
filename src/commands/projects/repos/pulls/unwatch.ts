66b03f553a5d081c225ae64a9eddcfbb56ab5528
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-06 00:16:56 +0200

    Created projects:repos:pulls:unwatch command

--- src/commands/projects/repos/pulls/unwatch.ts
+++ src/commands/projects/repos/pulls/unwatch.ts
@@ -3,11 +3,11 @@ import { StashConnector } from "stash-connector";
 import { BaseCommand, BuildFlags } from "../../../../libs/core/baseCommand";
 import { StashCLIResponse } from "../../../../libs/core/stashResponse";
 
-export default class Watch extends BaseCommand {
-    static description = 'Make the authenticated user watch the specified pull request.';
+export default class Unwatch extends BaseCommand {
+    static description = 'Make the authenticated user stop watching the specified pull request.';
     static examples = [
-        `$ stash projects:repos:pulls:watch -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234 --json`,
-        `$ stash projects:repos:pulls:watch -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234`,
+        `$ stash projects:repos:pulls:unwatch -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234 --json`,
+        `$ stash projects:repos:pulls:unwatch -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234`,
     ];
     static flags = {
         ...BaseCommand.flags,
@@ -32,9 +32,9 @@ export default class Watch extends BaseCommand {
         const response = new StashCLIResponse<any>();
         const connector = new StashConnector(this.localConfig.getConnectorOptions(this.flags.alias));
         try {
-            await connector.projects.repos(this.flags.project).pullRequests(this.flags.slug).watch(this.flags.pull).start();
+            await connector.projects.repos(this.flags.project).pullRequests(this.flags.slug).watch(this.flags.pull).stop();
             response.status = 0;
-            response.message = 'Start Watching Pull Request successfully';
+            response.message = 'Stop Watching Pull Request successfully';
             this.ux.log(response.message);
         } catch (error) {
             this.processError(response, error);
