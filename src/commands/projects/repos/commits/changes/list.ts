a1ffe4cb94671591d41df13b3c38de893f7fab8e
Juan José Longoria <juanjoselongoria@gmail.com>
2022-09-06 00:38:56 +0200

    Created projects:repos:commits:changes:list command

--- src/commands/projects/repos/commits/changes/list.ts
+++ src/commands/projects/repos/commits/changes/list.ts
@@ -6,11 +6,11 @@ import { RepoChangesColumns } from "../../../../../libs/core/tables";
 import { UX } from "../../../../../libs/core/ux";
 
 export default class List extends BaseCommand {
-    static description = 'Gets changes for the specified PullRequest. ' + UX.processDocumentation('<doc:RepoChangesOutput>');
+    static description = 'Retrieve a page of changes made in a specified commit. ' + UX.processDocumentation('<doc:RepoChangesOutput>');
     static examples = [
-        `$ stash projects:repost:pulls:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234 --all --csv`,
-        `$ stash projects:repost:pulls:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234 -l 100 -s 50 --json`,
-        `$ stash projects:repost:pulls:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --pull 1234 --from 5678 --limit 30`,
+        `$ stash projects:repos:commits:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --commit "a62ajdu128" --without-comments --all --csv`,
+        `$ stash projects:repos:commits:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --commit "a62ajdu128" -l 100 -s 50 --json`,
+        `$ stash projects:repos:commits:changes:list -a MyStashAlias --project "ProjectKey" --slug "MyRepoSlug" --commit "a62ajdu128" --since "ab4552nasj" --limit 30`,
     ];
     static flags = {
         ...BaseCommand.flags,
@@ -19,19 +19,24 @@ export default class List extends BaseCommand {
         alias: BuildFlags.alias,
         ...BuildFlags.pagination,
         project: Flags.string({
-            description: 'The Project key to retrieve repository pull requests changes',
+            description: 'The Project key to retrieve commit changes',
             required: true,
             name: 'Project'
         }),
         slug: Flags.string({
-            description: 'The Repository slug to retrieve the pull requests changes',
+            description: 'The Repository slug to retrieve commit changes',
             required: true,
             name: 'Slug'
         }),
-        pull: Flags.integer({
-            description: 'The Pull Request Id to retrieve changes',
+        commit: Flags.string({
+            description: 'The commit id to retrieve changes',
             required: true,
-            name: 'Pull Request Id',
+            name: 'Commit'
+        }),
+        since: Flags.string({
+            description: 'The commit to which until should be compared to produce a page of changes. If not specified the commit\'s first parent is assumed (if one exists)',
+            required: false,
+            name: 'Since'
         }),
         'without-comments': Flags.boolean({
             description: 'true to apply comment counts in the changes (the default); otherwise, false to stream changes without comment counts',
@@ -45,20 +50,32 @@ export default class List extends BaseCommand {
         try {
             let result: Page<RepoChangesOutput> = new Page();
             if (this.flags.all) {
-                let tmp = await connector.projects.repos(this.flags.project).pullRequests(this.flags.slug).changes(this.flags.pull).list(!this.flags['without-comments'], this.allPageOptions);
+                let tmp = await connector.projects.repos(this.flags.project).commits(this.flags.slug).changes(this.flags.commit).list({
+                    since: this.flags.since,
+                    withComments: !this.flags['without-comments'],
+                    pageOptions: this.allPageOptions
+                });
                 result.values.push(...tmp.values);
                 result.isLastPage = true;
                 result.start = tmp.start;
                 while (!tmp.isLastPage) {
-                    tmp = await connector.projects.repos(this.flags.project).pullRequests(this.flags.slug).changes(this.flags.pull).list(!this.flags['without-comments'], {
-                        start: tmp.nextPageStart,
-                        limit: 100,
+                    tmp = await connector.projects.repos(this.flags.project).commits(this.flags.slug).changes(this.flags.commit).list({
+                        since: this.flags.since,
+                        withComments: !this.flags['without-comments'],
+                        pageOptions: {
+                            start: tmp.nextPageStart,
+                            limit: 100,
+                        }
                     });
                     result.values.push(...tmp.values);
                 }
                 result.size = result.values.length;
             } else {
-                result = await connector.projects.repos(this.flags.project).pullRequests(this.flags.slug).changes(this.flags.pull).list(!this.flags['without-comments'], this.pageOptions);
+                result = await connector.projects.repos(this.flags.project).commits(this.flags.slug).changes(this.flags.commit).list({
+                    since: this.flags.since,
+                    withComments: !this.flags['without-comments'],
+                    pageOptions: this.pageOptions
+                });
             }
             response.result = result;
             response.status = 0;
